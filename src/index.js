089c4dd9be8025cf2778c18ccb2515a3e1ae8179
Matthew Berryman <matthew@acrossthecloud.net>
2024-01-16 18:17:57 +1030

    use actual data

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 
 import ApexCharts from 'apexcharts';
 
-var options = {
+let options = {
   chart: {
     type: 'line',
     animations: {
@@ -12,18 +12,43 @@ var options = {
       enabled: false // disable zooming because of Safari bug
     }
   },
-  series: [{
-    name: 'sales',
-    data: [30,40,35,50,49,60,70,91,125]
-  }],
   xaxis: {
-    categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+    type: 'datetime'
   },
   theme: {
     mode: 'dark' 
   }
 }
 
-var chart = new ApexCharts(document.querySelector("#chart"), options);
 
-chart.render();
+fetch('https://qyjbcjs0x7.execute-api.ap-southeast-2.amazonaws.com/dev/725c0cb7-cc43-4049-a908-e71e3679b547').then(res => res.json()).then(data => {
+
+  const teamNames = new Set(); 
+  let distanceDataByTeam ={};
+
+  // sort data by (new Date(event.start_date).getTime())
+  data.sort((a, b) => new Date(a.start_date).getTime() - new Date(b.start_date).getTime()).forEach(event => {
+    teamNames.add(event.team);
+    distanceDataByTeam[event.team] = distanceDataByTeam[event.team] || []
+    // calculate cumulative distance
+    if (distanceDataByTeam[event.team].length > 0) {
+      event.distance += distanceDataByTeam[event.team][distanceDataByTeam[event.team].length - 1][1];
+    }
+    distanceDataByTeam[event.team].push([new Date(event.start_date).getTime(), event.distance]);
+  });
+  console.log(data);
+  console.log(teamNames);
+  console.log(distanceDataByTeam);
+
+  const series = [];
+  teamNames.forEach(team => {
+    series.push({
+      name: team,
+      data: distanceDataByTeam[team]
+    });
+  });
+  options.series = series;
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+  chart.render();
+});
