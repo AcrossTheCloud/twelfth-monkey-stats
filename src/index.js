dca7eed8a6f52b45ea79baa1267b4c706ebcb908
Matthew Berryman <matthew@acrossthecloud.net>
2024-01-17 11:06:42 +1030

    responsive on mobile

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import './style.css';
 
 import ApexCharts from 'apexcharts';
 
+const API_URL = 'https://qyjbcjs0x7.execute-api.ap-southeast-2.amazonaws.com/dev/';
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id") || '725c0cb7-cc43-4049-a908-e71e3679b547';
+
 let options = {
   chart: {
     type: 'line',
@@ -13,22 +18,66 @@ let options = {
     }
   },
   xaxis: {
-    type: 'datetime'
+    type: 'datetime',
+    datetimeFormatter: {
+      year: 'yyyy',
+      month: "MMM 'yy",
+      day: 'dd MMM',
+      hour: 'HH:mm'
+    },
   },
   theme: {
-    mode: 'dark' 
+    mode: 'dark'
   },
   legend: {
     show: true,
-    position: 'top'
+    position: 'top',
+  },
+  tooltip: {
+    x: {
+      format: 'dd MMM yyyy'
+    },
+    y: {
+      formatter: function (value) {
+        return value.toFixed(2) + ' km';
+      }
+    },
+    style: {
+      fontSize: '11px'
+    }
   },
   markers: {
     size: 5,
   },
+  responsive: [{
+    breakpoint: 1000,
+    options: {
+      xaxis: {
+        labels: {
+          style: {
+            fontSize: '30px'
+          }
+        },
+      },
+      yaxis: {
+        labels: {
+          style: {
+            fontSize: '30px'
+          }
+        },
+      },
+      legend: {
+        fontSize: '30px'
+      },
+      markers: {
+        size: 10.
+      }
+    }
+  }]
 }
 
 
-fetch('https://qyjbcjs0x7.execute-api.ap-southeast-2.amazonaws.com/dev/725c0cb7-cc43-4049-a908-e71e3679b547').then(res => res.json()).then(data => {
+fetch(`${API_URL}${id}`).then(res => res.json()).then(data => {
 
   const teamNames = new Set(); 
   let distanceDataByTeam ={};
@@ -51,6 +100,10 @@ fetch('https://qyjbcjs0x7.execute-api.ap-southeast-2.amazonaws.com/dev/725c0cb7-
       data: distanceDataByTeam[team]
     });
   });
+  const width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+  if (width < 1000) {
+    options.tooltip.style.fontSize = '30px';
+  }
   options.series = series;
   var chart = new ApexCharts(document.querySelector("#chart"), options);
 
