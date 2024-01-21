7089a0c711725d6e4a023c7af9929a57bdd37b43
Matthew Berryman <matthew@acrossthecloud.net>
2024-01-22 08:26:54 +1030

    add CSV and copy it to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// pass through all csv files
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -9,7 +12,12 @@ module.exports = {
       title: 'Twelfth Monkey stats',
       // Load a custom template (lodash by default)
       template: 'src/index.html'
-    })
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/CSVs", to: "CSVs" },
+      ]
+    }),
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -24,6 +32,9 @@ module.exports = {
     hot: true,
     compress: true,
     historyApiFallback: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
   },
   module: {
     rules: [
